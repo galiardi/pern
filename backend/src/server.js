26d2d7e7fe67e82bc7d4ad662edde6cbe4deb21a
galiardi <galiardi.dev@gmail.com>
2022-08-28 07:11:09 -0400

    backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
