f71ba11130402bdbecf3e537a1ba79fa7683c26b
galiardi <galiardi.dev@gmail.com>
2022-08-31 11:35:32 -0400

    Add enviroment variables. Add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const tasksRouter = require("./routes/tasks.routes");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
 app.use("/tasks", tasksRouter);
+
 app.use((error, req, res, next) => {
   return res.json({
     error: error.message,
